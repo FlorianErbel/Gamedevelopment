f0227668dba1b38f2a725237b5fc62038246a316
FlorianErbel <105227799+FlorianErbel@users.noreply.github.com>
2026-01-15 17:01:30 +0100

    Upload all

--- doodle.p8
+++ doodle.p8
@@ -4,6 +4,7 @@ __lua__
 #include src/Util.lua
 #include src/Camera.lua
 #include src/platforms/Platform.lua
+#include src/platforms/PlatformManager.lua
 #include src/platforms/BreakablePlatform.lua
 #include src/platforms/CatapultPlatform.lua
 #include src/Player.lua
---
+++ src/platforms/Platform.lua
@@ -0,0 +1,38 @@
+---
+--- Platform class
+--- Created by florianerbel
+--- DateTime: 15.01.26 15:39
+--- Template class to enable using the template pattern
+---
+
+---@class Platform
+---@field public name string
+local Platform = {}
+Platform.__index = Platform
+
+---Constructor
+---@param name string
+---@return Platform
+function Platform.new(name)
+    local self = setmetatable({}, Platform)
+      self.x = x
+      self.y = y
+      self.w = w
+      self.h = 4
+      self.dead = false
+      return self
+end
+
+function Platform:on_land(player)
+  -- Default: nichts
+end
+
+function Platform:update()
+end
+
+function Platform:draw()
+    rectfill(self.x, self.y, self.x + self.w - 1, self.y + self.h - 1, 11)
+    rect(self.x, self.y, self.x + self.w - 1, self.y + self.h - 1, 3)
+end
+
+---return Platform
