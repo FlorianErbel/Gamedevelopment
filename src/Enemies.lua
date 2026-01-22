19c8a8aedb28c170b789f7d35b03ad51d413070a
FlorianErbel <105227799+FlorianErbel@users.noreply.github.com>
2026-01-22 08:03:59 +0100

    TODO eingesetzt

--- src/Enemies.lua
+++ src/Enemies.lua
@@ -33,6 +33,7 @@ function enemies:try_spawn_on_platform(platform, difficulty, height)
     if platform.ground then return end
     if platform.w < 18 then return end -- zu klein für igel-laufen
 
+-- TODO: p = player, propability oder plat?
     local p = self:spawn_prob(difficulty, height)
 
     -- TODO: Wenn wir die Kontrolle umdrehen und den folgenden Code-Block hier einbauen, kommen wir dann nicht zum selben Erbenis, nur ohne den Funktionsabbruch (return)
