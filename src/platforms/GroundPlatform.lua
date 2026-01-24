2a892f9c2846e7798f353d5af71a7dd4b9dfb1cb
FlorianErbel <105227799+FlorianErbel@users.noreply.github.com>
2026-01-24 15:10:57 +0100

    Dokumentation inklusive Typannotationen erstellt

--- src/platforms/GroundPlatform.lua
+++ src/platforms/GroundPlatform.lua
@@ -1,22 +1,26 @@
 ---
---- GroundPlatform class
---- Created by florianerbel
---- DateTime: 15.01.26 20:56
+--- Plattformtyp für den Boden (Startplattform).
+--- Erbt von Platform und stellt eine höhere, visuell unterscheidbare Plattform dar.
 ---
-
----@class GroundPlatform
----@field public name string
+---@class GroundPlatform : Platform
 local GroundPlatform = {}
-GroundPlatform = setmetatable({}, Platform)
 GroundPlatform.__index = GroundPlatform
+setmetatable(GroundPlatform, Platform)
 
----Constructor
----@param name string
+---
+--- Erstellt eine neue Bodenplattform.
+--- Diese Plattform ist höher als normale Plattformen und dient als Startbereich.
+---
+---@param pos_x number
+---@param pos_y number
+---@param width number
 ---@return GroundPlatform
 function GroundPlatform.new(pos_x, pos_y, width)
     local self = Platform.new(pos_x, pos_y, width)
+
+    self.height = 8
     self.fill_color = 4
     self.border_color = 3
-    self.height = 8
+
     return setmetatable(self, GroundPlatform)
 end
